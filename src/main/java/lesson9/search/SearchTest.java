54ff9d8ee6f369ef77c2abb7c728e43ba3a8b185
Oleksii Pohasii <pogasiya@gmail.com>
2023-06-03 21:30:20 +0200

    update

--- src/main/java/lesson9/search/SearchTest.java
+++ src/main/java/lesson9/search/SearchTest.java
@@ -1,12 +1,14 @@
 package lesson9.search;
 
+import lesson9.sort.SortUtils;
+
 import java.util.Arrays;
 
 public class SearchTest {
 
     public static void main(String[] args) {
 
-        int[] array = {3, 7, 1, 3, 9,2,14,78,16,12,0,7,2, 50};
+        int[] array = {3, 7, 1, 3, 9, 2, 14, 78, 16, 12, 0, 7, 2, 50};
 
         System.out.println("Результат линейноо поиска: ");
         System.out.println(SearchUtils.linearSearch(array, 78));
@@ -17,6 +19,32 @@ public class SearchTest {
         System.out.println(Arrays.toString(array));
 
         System.out.println("Результат двоичного поиска: ");
-        System.out.println(SearchUtils.binarySearch(array, 100));
+        System.out.println(SearchUtils.binarySearch(array, 14));
+
+        System.out.println("Результат поиска прыжками: ");
+        System.out.println(SearchUtils.jumpSearch(array, 14));
+
+        array = new int[]{3, 7, 1, 3, 9, 2, 14, 78, 16, 12, 0, 7, 2, 50};
+
+        System.out.println("Исходный неотсортированный массив:");
+        System.out.println(Arrays.toString(array));
+
+        SortUtils.bubbleSort(array);
+        System.out.println("Сортировка методом пузырька:");
+        System.out.println(Arrays.toString(array));
+
+        System.out.println("Исходный неотсортированный массив:");
+        System.out.println(Arrays.toString(array));
+        SortUtils.insertionSort(array);
+        System.out.println("Сортировка вставками:");
+        System.out.println(Arrays.toString(array));
+
+        System.out.println("Исходный неотсортированный массив:");
+        System.out.println(Arrays.toString(array));
+        SortUtils.selectionSort(array);
+        System.out.println("Сортировка выбором");
+        System.out.println(Arrays.toString(array));
+
+
     }
 }
